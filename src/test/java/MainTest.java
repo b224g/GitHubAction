e182ccf1b9a7f6a889716f2ca75e5e13f136b4f9
biro224 <boubacarbirobarry837@gmail.com>
2022-11-17 00:12:39 +0300

    add Mult

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -15,4 +15,12 @@ public class MainTest {
         assertEquals(1,main.diff(3,2));
 
     }
+    void Multiplication()
+    {
+        assertEquals(1,main.Mult(2,5));
+    }
+    /*  void division()
+    {
+        assertEquals(1,main.div(3,6));
+    }*/
 }
